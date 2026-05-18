c75c73670626ca62e0c958d4e57584563c5bf851
Luka Stojiljkovic <luka.d.stojiljkovic@gmail.com>
2026-05-18 14:25:32 +0200

    feat: trading-service prazan Spring Boot shell

---
+++ trading-service/src/main/java/rs/raf/trading/TradingServiceApplication.java
@@ -0,0 +1,12 @@
+package rs.raf.trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradingServiceApplication.class, args);
+    }
+}
---
+++ trading-service/src/test/java/rs/raf/trading/TradingServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package rs.raf.trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
